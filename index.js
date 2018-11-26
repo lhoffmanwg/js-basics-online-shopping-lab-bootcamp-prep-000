4d6648247ee0c16c5f74bbeeaf44b66ca23cc6f6
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-26 02:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = 0;
+  var cart = [];
+  price = Math.random;
+  cart.push("{ itemName: " + item +", itemPrice: "+ price);
+  return (`${item} has been added to your cart.`);
+ 
 }
 
 function viewCart() {
@@ -26,5 +31,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var i = 0;
+  if(cardNumber===null){
+     return ("Sorry, we don't have a credit card on file for you.");
+  }else{
+    for(i = 0; i < cart.length; i++){
+      cart.pop;
+    }
+    return (`Your total cost is ${total}, which will; be charged to ${cardNumber}.`);
+  }
 }
